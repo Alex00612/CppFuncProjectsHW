1b5e483b4ebe220ed81c49b2606685328f641314
Alex00612 <149431488+Alex00612@users.noreply.github.com>
2023-10-31 10:43:57 +0300

    Add files via upload

---
+++ 7 integers.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int a;
+	cout << "Введите число: ";
+	cin >> a;
+	int b = a;
+	for (int i = 1; i < 7; i++) {
+		cout << "Введите число: ";
+		cin >> a;
+		if (b < a) b = a;
+	}
+	cout << "макс: " << b;
+}
