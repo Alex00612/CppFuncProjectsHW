9fc7384fa3cdd94499d09e8bdbec07fcaec2c1a8
Alex00612 <149431488+Alex00612@users.noreply.github.com>
2023-10-31 11:15:37 +0300

    Add files via upload

---
+++ salary for the quarter.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+	setlocale(LC_ALL, "");
+	float a, b, c;
+	cout << "Введите оценку за 1 месяц: ";
+	cin >> a;
+	cout << "Введите оценку за 2 месяц: ";
+	cin >> b;
+	cout << "Введите оценку за 3 месяц: ";
+	cin >> c;
+	cout << a + b + c;
+}
