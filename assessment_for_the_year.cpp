51e7564be109360e2ebd089324e10b8d25cb1f82
Alex00612 <149431488+Alex00612@users.noreply.github.com>
2023-10-31 10:14:50 +0300

    Add files via upload

---
+++ assessment_for_the_year.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a, b, c, d;
+	cout << "Введите оценку за 1 четверть: ";
+	cin >> a;
+	cout << "Введите оценку за 2 четверть: ";
+	cin >> b;
+	cout << "Введите оценку за 3 четверть: ";
+	cin >> c;
+	cout << "Введите оценку за 4 четверть: ";
+	cin >> d;
+	cout << round ((a + b + c + d)/4);
+
+
+}
